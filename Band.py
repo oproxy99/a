01be5f7ac498d7c2771415e476d95db769cc38fc
Rahul Bikram Neupane <109612047+oproxy99@users.noreply.github.com>
2022-07-27 01:11:11 +0545

    day1
    band name generator

---
+++ Band.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city=input("Which city were you born in?\n")
+pet=input("What was the name of your first pet?\n")
+print(f"The name of your band could be: {city} {pet}")
